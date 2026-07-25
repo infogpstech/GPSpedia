1cc7fade0a7da26fa56c150d4852f7d7785f5076
GPSpedia <gpsinfotecno@gmail.com>
2026-07-25 12:53:05 -0600

    Update WRITE and ADMIN API endpoints

--- api-config.js
+++ api-config.js
@@ -8,11 +8,11 @@ export const API_ENDPOINTS = {
     LEGACY: "https://script.google.com/macros/s/AKfycbwpIFH1nX2BZEjAKbpq9HJpEGNlo_0LqD1CwxWsWFo5J0AJDdbfMrKpcsAV4ZFJzFWd/exec",
     AUTH:     "https://script.google.com/macros/s/AKfycbwATstMSSnuYZMeGEjI7Q5cznO6kA8rqLo7zNZLmu_f29qwcyt4Fucn5VIBdB9tMoRg/exec",
     CATALOG:  "https://script.google.com/macros/s/AKfycbwDLzfOoVGypw51bG_ISSsvETtDGdpOBv_7OkX82GgMh13cVnVoHRyEqW_TU6r7m7-2Kg/exec",
-    WRITE:    "https://script.google.com/macros/s/AKfycbx-a6yvMX5IbxTCCUpPCS0sb0iDGkA2iUkLXMdTzJf9kuCbZGfqLQTQWtOq4xN_bANW/exec",
+    WRITE:    "https://script.google.com/macros/s/AKfycbzpzg0U3vMA8TTypaqhsc4Jm8stKsNqQ345HPpCGuKWH6EUNZ1Ue91wtJEmr6Czyr8D/exec",
     USERS:    "https://script.google.com/macros/s/AKfycbz6KfY_E2d-efAYBXir_UGjUUPIggDJkJGUboqedO4eWFF9y3vr4IQzveeg563FLjs/exec",
     FEEDBACK: "https://script.google.com/macros/s/AKfycbz3NzT3FPn40VDbc7irIF632xHoeopM0GdnCIim4APAYTzb9KYDjZWDXHHrtzq4atZ70Q/exec",
     UTILITIES: "https://script.google.com/macros/s/AKfycbzkGXk_kSm3rN7K5PM0RntiPAn7DlH78RkH66a2vuwZwU8KgwDufkOiPjXoUKzuHAgG/exec",
-    ADMIN: "https://script.google.com/macros/s/AKfycbwPz26R9qD7y0lEcl-3WJgLzF87K62H63bX4y82Dug47fBgwCyfL6b2WjH_f26M8Y0gK8g/exec"
+    ADMIN: "https://script.google.com/macros/s/AKfycbwwfOFBxV2MBHl90r7f8NQYIkA5CdEZeCvNri0bbEpwVY280bmrDuu9JQZPCi9sX9r7/exec"
 };
 
 export const ACTION_TO_SERVICE_MAP = {
